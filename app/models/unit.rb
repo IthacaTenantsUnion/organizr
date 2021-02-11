b6e311ddaf024e442c6bd60ba68eb2b0ab573a47
Aaron Hill <armahillo@gmail.com>
2021-02-11 10:33:20 -0500

    Adds enum to Unit

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -2,10 +2,12 @@
 #
 # Table name: units
 #
-#  id         :bigint           not null, primary key
-#  address    :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                  :bigint           not null, primary key
+#  address             :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  number_of_occupants :integer          default(1)
+#  building_type       :string
 #
 class Unit < ApplicationRecord
   has_many :tenancies, -> { extending AverageRatings }
@@ -15,4 +17,11 @@ class Unit < ApplicationRecord
 
   validates :address, presence: true, uniqueness: true
   validates :number_of_occupants, numericality: { greater_than: 0 }
+
+  enum building_type: {
+    single_unit: "Single-unit house",
+    multi_unit: "Multi-unit (converted) house",
+    apt_complex: "Apartment complex",
+    program_housing: "Program housing"
+  }
 end
--- spec/models/unit_spec.rb
+++ spec/models/unit_spec.rb
@@ -2,10 +2,12 @@
 #
 # Table name: units
 #
-#  id         :bigint           not null, primary key
-#  address    :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                  :bigint           not null, primary key
+#  address             :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  number_of_occupants :integer          default(1)
+#  building_type       :string
 #
 require 'rails_helper'
 
