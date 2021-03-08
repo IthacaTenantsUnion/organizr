7ab24645d5826be6771405bacb3960b80e51100f
Aaron Hill <armahillo@gmail.com>
2021-03-07 20:09:41 -0500

    Fixes 500 on the Edit user profile page
    Fixes #36
    
    Devise uses weird controller actions so it broke the shared back button.

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  let(:user) { create(:user) }
 
+  context "while signed in as a tenant" do
+    before do
+      sign_in user
+    end
+
+    describe 'GET :edit' do
+      context "while editing yourself" do
+        subject { get edit_user_registration_url(user) }
+        it { is_expected.to eq 200 }
+      end
+
+      context "when editing someone who isn't you" do
+        let!(:other_user) { create(:user) }
+        subject { get edit_user_registration_url(other_user) }
+        it { is_expected.not_to eq 401 }
+      end
+    end
+  end
+  
 end
